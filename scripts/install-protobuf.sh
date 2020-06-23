d32973652909008561e63ff07bfc17b76c1efc73
Kirill Sulim <kirillsulim@gmail.com>
2020-06-23 20:56:14 +0500

    Extract protoc locally

--- scripts/install-protobuf.sh
+++ scripts/install-protobuf.sh
@@ -5,5 +5,5 @@ PROTOC_VERSION=3.12.3
 ARCHIVE=protoc-${PROTOC_VERSION}-linux-x86_64.zip
 
 wget https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VERSION}/${ARCHIVE}
-unzip -o ${ARCHIVE} -d /usr/local bin/protoc
+unzip -o ${ARCHIVE} -d . bin/protoc
 rm -f ${ARCHIVE}
